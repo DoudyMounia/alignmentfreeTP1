2c8edc290540ad1b9b59bbd6cde9d8859428aa9f
DoudyMounia <117927178+DoudyMounia@users.noreply.github.com>
2022-11-16 17:15:46 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -1,4 +1,3 @@
-
 def kmer2str(val, k):
     """ Transform a kmer integer into a its string representation
     :param int val: An integer representation of a kmer
@@ -15,6 +14,51 @@ def kmer2str(val, k):
     return "".join(str_val)
 
 
+
+
+def encode(letter):
+    #letters={'A':0, 'C':1, 'T': 2, 'G':3}
+    letters = ['A', 'C', 'T', 'G']
+    
+    return letters.index(letter)
+
+   
+text='AGGTC' 
+
+k=2   
+
+def mask(k):
+    return( (1<< 2*(k-1))-1)
+
+
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+
+    #encodage de la 1ere lettre:
+    kmer=0
+    for i in range (k-1):
+        #print(kmer)
+        kmer=kmer<<2
+        kmer=kmer+ encode(text[i])
+        print(kmer)
+        
+    #encodage du reste du text:
+    list_kmer=[]
+    for i in text[k-1:]:
+        kmer=kmer & mask(k)
+        kmer=kmer<<2
+        kmer+=encode(i)
+        print(kmer)
+        #rkmer=kmer
+        #rkmer=rkmer>>2
+        
+        list_kmer.append(kmer)
+        
+    return list_kmer
+
+
+
+
+a=stream_kmers('TGTA', 3)
+print (a)
+for j in a:
+    print(kmer2str(j, k))
